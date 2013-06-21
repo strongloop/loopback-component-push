e59d197f1bf23350fddda4948c7a11b63066a821
Raymond Feng <raymond@strongloop.com>
2013-06-21 16:12:41 -0700

    delete all records before create

--- example/registration.js
+++ example/registration.js
@@ -7,17 +7,21 @@ app.use(asteroid.rest());
 var model = require('../models/device-registration');
 app.model(model);
 
-model.create({
-    appId: 'MyAsteroidApp',
-    userId: 'MyAsteroidUser',
-    deviceToken: '75624450 3c9f95b4 9d7ff821 20dc193c a1e3a7cb 56f60c2e f2a19241 e8f33305',
-    deviceType: 'apns',
-    created: new Date(),
-    lastModified: new Date(),
-    status: 'Active'
-}, function(err, result) {
-
+model.destroyAll(function (err, result) {
+    console.log('Adding a test record');
+    model.create({
+        appId: 'MyAsteroidApp',
+        userId: 'MyAsteroidUser',
+        deviceToken: '75624450 3c9f95b4 9d7ff821 20dc193c a1e3a7cb 56f60c2e f2a19241 e8f33305',
+        deviceType: 'apns',
+        created: new Date(),
+        lastModified: new Date(),
+        status: 'Active'
+    }, function (err, result) {
+        console.log('Registration record is created: ', result);
+    });
 });
 
 // start the server
 app.listen(3000);
+console.log('Server is ready at http://127.0.0.1:3000/deviceRegistrations');
