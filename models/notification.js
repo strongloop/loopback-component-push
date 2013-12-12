f69ed8b792c150bb3c9c973d63f42fb8cc08766e
Miroslav Bajtos <miroslav@strongloop.com>
2013-12-12 17:02:07 +0100

    Notification: fix jshint warnings
    Fix indentation, wrap too long line comments into jsdoc blocks.

--- models/notification.js
+++ models/notification.js
@@ -10,66 +10,101 @@ var loopback = require('loopback');
  * [iOS (APN)]{@link http://bit.ly/apns-notification-payload}
  */
 var Notification = loopback.createModel(
-    'Notification',
-    {
-        id: {
-            type: String,
-            id: true,
-            generated: true
-        },
-        deviceType: String, // The device type such as apns
-        deviceToken: String, // The device token
+  'Notification',
+  {
+    id: {
+      type: String,
+      id: true,
+      generated: true
+    },
 
-        /**
-         * (iOS only)
-         * The notification's message.
-         */
-        alert: 'any',
+    /**
+     * The device type such as `apns`.
+     */
+    deviceType: String,
 
-        /**
-         * (iOS only)
-         * The value indicated in the top right corner of the app icon.
-         * This can be set to a value or to -1 in order to increment
-         * the current value by 1.
-         */
-        badge: Number,
+    /**
+     * The device token as provided by GCM/APN/etc.
+     */
+    deviceToken: String,
 
-        /**
-         * (iOS only)
-         * The name of a sound file in the application bundle
-         */
-        sound: String,
+    /**
+     * (iOS only)
+     * The notification's message.
+     */
+    alert: 'any',
 
-        /**
-         * (Android only)
-         * An arbitrary string (such as "Updates Available") that is used
-         * to collapse a group of like messages when the device is offline,
-         * so that only the last message gets sent to the client.
-         */
-        collapseKey: String,
+    /**
+     * (iOS only)
+     * The value indicated in the top right corner of the app icon.
+     * This can be set to a value or to -1 in order to increment
+     * the current value by 1.
+     */
+    badge: Number,
 
-        /**
-         * (Android only)
-         * Indicates that the message should not be sent immediately
-         * if the device is idle. The server will wait for the device
-         * to become active, and then only the last message for each
-         * collapse_key value will be sent.
-         */
-        delayWhileIdle: Boolean,
+    /**
+     * (iOS only)
+     * The name of a sound file in the application bundle.
+     */
+    sound: String,
 
-        created: Date, // The date that the notification is created
-        modified: Date, // The date that the notifcation is modified
-        scheduledTime: Date, // The time that the notification should be sent
-        expirationTime: Date, // The time that the notification should be expired
-        expirationInterval: Number, // The expiration interval in seconds after the scheduled time
-        status: String // The status of the notification
-    }
+    /**
+     * (Android only)
+     * An arbitrary string (such as "Updates Available") that is used
+     * to collapse a group of like messages when the device is offline,
+     * so that only the last message gets sent to the client.
+     */
+    collapseKey: String,
+
+    /**
+     * (Android only)
+     * Indicates that the message should not be sent immediately
+     * if the device is idle. The server will wait for the device
+     * to become active, and then only the last message for each
+     * collapse_key value will be sent.
+     */
+    delayWhileIdle: Boolean,
+
+    /**
+     * The date that the notification is created.
+     */
+    created: Date,
+
+    /**
+     *The date that the notifcation is modified.
+     */
+    modified: Date,
+
+    /**
+     * The time that the notification should be sent.
+     * (not supported yet)
+     */
+    scheduledTime: Date,
+
+    /**
+     * The time that the notification should be expired.
+     */
+    expirationTime: Date,
+
+    /**
+     * The expiration interval in seconds.
+     * The interval starts at the time when the notification is sent
+     * to the push notification provider.
+     */
+    expirationInterval: Number,
+
+    /**
+     * The status of the notification.
+     * (not supported yet)
+     */
+    status: String
+  }
 );
 
-Notification.beforeCreate = function (next) {
-    var notification = this;
-    notification.created = notification.modified = notification.scheduledTime = new Date();
-    next();
+Notification.beforeCreate = function(next) {
+  var notification = this;
+  notification.created = notification.modified = notification.scheduledTime = new Date();
+  next();
 };
 
 Notification.prototype.getTimeToLiveInSecondsFromNow = function() {
