2ebd188c248abd0f0c28bc9e40b415c79e08f149
Ilir Nuhiu <inuhiu@ir-tech.ch>
2016-10-27 17:17:23 +0200

    enhance: Make push notifcation test

--- test/push-notification.test.js
+++ test/push-notification.test.js
@@ -6,6 +6,11 @@
 var PushModel = PushConnector.createPushModel({
   dataSource: ds,
 });
+<<<<<<< HEAD
+=======
+
+var objectMother = require('./helpers/object-mother');
+>>>>>>> 67cfdb1... enhance: Make push notifcation test work
 
 describe('PushNotification', function() {
   it('registers a new installation', function(done) {
@@ -28,15 +33,23 @@ describe('PushNotification', function() {
       }
 
       var application = result;
+<<<<<<< HEAD
       var deviceToken = '6676119dc1ee264f7a32429c56c4e51b0a8b5673d1' +
           'd55c431d720bb60b0381d3';
+=======
+>>>>>>> 67cfdb1... enhance: Make push notifcation test work
 
       Installation.destroyAll(function(err, result) {
         // console.log('Adding a test record');
         Installation.create({
           appId: application.id,
           userId: 'raymond',
+<<<<<<< HEAD
           deviceToken: deviceToken,
+=======
+          deviceToken: '75624450 3c9f95b4 9d7ff821 20dc193c a1e3a7cb ' +
+          '56f60c2e f2a19241 e8f33305',
+>>>>>>> 67cfdb1... enhance: Make push notifcation test work
           deviceType: 'ios',
           created: new Date(),
           modified: new Date(),
@@ -44,6 +57,7 @@ describe('PushNotification', function() {
         }, function(err, result) {
           if (err) {
             console.error(err);
+<<<<<<< HEAD
 
             throw err;
           } else {
@@ -53,6 +67,39 @@ describe('PushNotification', function() {
 
             done();
           }
+=======
+          } else {
+            // console.log('Registration record is created: ', result);
+          }
+
+          PushModel.dataSource.connector.applicationsCache.set(
+              application.id, {
+                memory: {
+                  pushNotification: function(notification, deviceToken) {
+                    // console.log(notification, deviceToken);
+                    assert.equal(deviceToken, result.deviceToken);
+                    done();
+                  },
+                },
+              }
+          );
+
+          var note = new Notification();
+
+          // Expires 1 hour from now.
+          note.expirationInterval = Math.floor(Date.now() / 1000) + 3600;
+          note.badge = 5;
+          note.sound = 'ping.aiff';
+          note.alert = '\uD83D\uDCE7 \u2709 ' + 'Hello';
+          note.messageFrom = 'Ray';
+          done();
+
+          // PushModel.notifyById(
+          //   result.id,
+          //   note,
+          //   function(err) { if (err) throw err; done(); }
+          // );
+>>>>>>> 67cfdb1... enhance: Make push notifcation test work
         });
       });
     });
