9ddb8c7dd065a166b9de8d6bc2f20b3700cabf08
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 09:41:24 -0400

    chore: update copyrights years

--- test/helpers/mockery/apns.mockery.js
+++ test/helpers/mockery/apns.mockery.js
@@ -1,7 +1,8 @@
-// Copyright IBM Corp. 2013. All Rights Reserved.
+// Copyright IBM Corp. 2013,2016. All Rights Reserved.
 // Node module: loopback-component-push
 // This file is licensed under the Artistic License 2.0.
 // License text available at https://opensource.org/licenses/Artistic-2.0
+
 'use strict';
 
 // This module provides a mocked environment where APN connections
